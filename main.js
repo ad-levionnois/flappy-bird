5449bd49c4ebef9f198ca94d6e50211c1422df72
ad-levionnois <adeline.levionnois74@gmail.com>
2020-06-30 14:11:58 +0200

    mofifications states

--- main.js
+++ main.js
@@ -1,9 +1,39 @@
 const cvs = document.getElementById('bird');
 const ctx = cvs.getContext('2d');
 
+// LOAD SPRITE IMG
 const sprite = new Image();
   sprite.src="sprite.png";
 
+let frames = 0;
+
+// GAME STATE
+const state = {
+  current : 0,
+  getReady : 0,
+  game : 1,
+  gameOver : 2,
+}
+
+// CONTROL GAME
+cvs.addEventListener("click",function(evt){
+  switch (state.current) {
+    case state.getReady:
+      state.current = state.game;
+      break;
+    case state.game:
+      state.current = state.gameOver;
+      break;
+    case state.gameOver:
+      state.current = state.getReady;
+      break;
+    default:
+
+  }
+
+});
+
+// BACKGROUND
 const bg = {
   sX : 0,
   sY : 0,
@@ -31,6 +61,9 @@ const fg = {
     ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
   }
 }
+
+// STATE
+
 const getReady ={
   sX : 0,
   sY : 229,
@@ -40,9 +73,12 @@ const getReady ={
   y : cvs.height -165,
 
   draw : function(){
-    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x/2, this.y/2, this.w, this.h);
+    if (state.current == state.getReady){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x/2, this.y/2, this.w, this.h);
+    }
   }
 }
+
 const gameOver={
   sX : 173,
   sY : 229,
@@ -52,34 +88,37 @@ const gameOver={
   y  : cvs.height -201,
 
   draw : function(){
-    ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x/2, this.y/2, this.w, this.h);
+    if (state.current == state.gameOver){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x/2, this.y/2, this.w, this.h);
+    }
   }
-
 }
+
+
+// BIRD
 const bird = {
-animation : [
-{sX : 277 , sY : 112 },
-{sX : 277 , sY : 139 },
-{sX : 277 , sY : 164 },
-{sX : 277 , sY : 139 }
-],
+  animation : [
+  {sX : 277 , sY : 112 },
+  {sX : 277 , sY : 139 },
+  {sX : 277 , sY : 164 },
+  {sX : 277 , sY : 139 }
+  ],
 
-w :33,
-h :27,
-x :50,
-y :150,
+  w :33,
+  h :27,
+  x :50,
+  y :150,
 
-frame :0,
+  frames :0,
 
-draw : function(){
-  let bird = this.animation [this.frame];
+  draw : function(){
+    let bird = this.animation [this.frame];
 
-  ctx.drawImage(sprite,bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y -this.h/2, this.w, this.h);
+    ctx.drawImage(sprite,bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y -this.h/2, this.w, this.h);
+  }
 }
 
-}
-
-
+// DRAW
 function draw(){
   bg.draw();
   fg.draw();
@@ -90,11 +129,8 @@ function draw(){
 draw();
 
 function loop () {
-  draw();
-  requestAnimationFrame(loop);
-
   frame++;
-
+  requestAnimationFrame(loop);
 }
 
 loop();
